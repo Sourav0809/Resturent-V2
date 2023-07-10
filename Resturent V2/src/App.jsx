3c5237934ca41f5dec641551e397613fddbbceae
Sourav0809 <spathak7431@gmail.com>
2023-07-10 11:20:49 +0530

    Cover section added

--- Resturent V2/src/App.jsx
+++ Resturent V2/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
+import Summary from "./Components/Summary/Summary";
 function App() {
   return (
     <>
       <Header />
+      <Summary />
     </>
   );
 }
---
+++ Resturent V2/src/Components/Summary/Summary.jsx
@@ -0,0 +1,11 @@
+import "./Summary.css";
+const Summary = () => {
+  return (
+    <div className="summary_section">
+      <h2>It's Not just Food , Its an Experience</h2>
+      <p>Fresh And Delicious Food Deliver To Your Doorstep Within 10 Minutes</p>
+    </div>
+  );
+};
+
+export default Summary;
