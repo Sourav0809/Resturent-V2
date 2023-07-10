02a2d96037f6f775d4818a5aa3d4b614e263fa2c
Sourav0809 <spathak7431@gmail.com>
2023-07-10 10:32:40 +0530

    first commit

---
+++ Resturent V2/src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+const Header = () => {
+  return (
+    <header className="header">
+      <div>
+        <h2 onClick={() => location.reload()}>Kolkata Local</h2>
+      </div>
+      <div className="cart-header">
+        <h2>
+          <i className="bx bx-cart"></i>
+        </h2>
+        <h2>Your Cart</h2>
+        <h2 className="cart-quantity">0</h2>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
