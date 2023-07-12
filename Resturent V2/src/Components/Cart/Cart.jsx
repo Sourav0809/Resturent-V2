41bd9f586ebe10d9e0ded5466f55cf18f94e818e
Sourav0809 <spathak7431@gmail.com>
2023-07-12 16:11:04 +0530

    Cart Container style added

--- Resturent V2/src/Components/Cart/Cart.jsx
+++ Resturent V2/src/Components/Cart/Cart.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import "./Cart.css";
 import Modal from "../UI/Modal";
 import CartContext from "../Store/CartContext";
@@ -11,27 +12,47 @@ const Cart = (props) => {
       {cartItems.map((items) => {
         return (
           <div key={items.addedItemId}>
-            <h1>{items.addedItemName}</h1>
-            <h1>{items.addedItemPrice}</h1>
-            <h1>{items.addedItemQuantity}</h1>
-            <button
-              onClick={() => {
-                cartCtx.incrementCartItem(items.addedItemId);
-              }}
-            >
-              Inc
-            </button>
-            <button
-              onClick={() => {
-                cartCtx.decrementCartItem(items.addedItemId);
-              }}
-            >
-              Dec
-            </button>
+            <div className="cart-items-container">
+              <div className="cart-item-image-name">
+                <div className="cart-image-container">
+                  <img src={items.addedItemImage} alt="logo" />
+                </div>
+                <div className="cart-item-name">
+                  <h2>{items.addedItemName}</h2>
+                </div>
+              </div>
+              <div className="cart-item-quantity">
+                <button
+                  className="cart-qunatity-btn"
+                  onClick={() => {
+                    cartCtx.incrementCartItem(items.addedItemId);
+                  }}
+                >
+                  +
+                </button>
+                <h2> &#x2716; {items.addedItemQuantity}</h2>
+                <button
+                  className="cart-qunatity-btn"
+                  onClick={() => {
+                    cartCtx.decrementCartItem(items.addedItemId);
+                  }}
+                >
+                  -
+                </button>
+              </div>
+              <div className="cart-item-price">
+                <h2>${items.addedItemPrice}</h2>
+              </div>
+            </div>
+            <hr />
           </div>
         );
       })}
-      <h1>Total Price {cartCtx.totalAmount}</h1>
+      <div className="total-amount-container">
+        <h1>Total Price</h1>
+        <h1>${cartCtx.totalAmount}</h1>
+      </div>
+
       <div className="cart-btn">
         <button className="order-btn">Order</button>
         <button onClick={props.onCartHide} className="close-btn">
